62331298f321c23d8913088dacda5b04e161bcd6
chnliquan <chnliquan@163.com>
2025-03-16 12:01:19 +0800

    feat(utils): add file loaders

--- packages/utils/src/file/loader.ts
+++ packages/utils/src/file/loader.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import importFresh from 'import-fresh'
+import yaml from 'js-yaml'
 import { dirname } from 'node:path'
 import { pathToFileURL } from 'node:url'
+import parseJson from 'parse-json'
 import {
   type TranspileOptions,
   findConfigFile,
@@ -18,23 +21,39 @@ import { remove, removeSync } from './remove'
 import { writeFile, writeFileSync } from './write'
 
 /**
- * 加载 js 文件
- * @param path 文件路径
+ * 默认异步加载器
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function loadJsSync<T = any>(path: string): T {
-  return importFresh(path)
-}
+export const defaultLoaders = Object.freeze({
+  '.mjs': loadJs,
+  '.cjs': loadJs,
+  '.js': loadJs,
+  '.ts': loadTs,
+  '.json': loadJson,
+  '.yaml': loadYaml,
+  '.yml': loadYaml,
+} as const)
+
+/**
+ * 默认同步加载器
+ */
+export const defaultLoadersSync = Object.freeze({
+  '.cjs': loadJsSync,
+  '.js': loadJsSync,
+  '.ts': loadTsSync,
+  '.json': loadJsonSync,
+  '.yaml': loadYamlSync,
+  '.yml': loadYamlSync,
+} as const)
 
 /**
  * 加载 js 文件
  * @param path 文件路径
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function loadJs<T = any>(path: string): Promise<T> {
+export async function loadJs<T>(path: string): Promise<T> {
   try {
     const { href } = pathToFileURL(path)
-    return (await import(href)).default
+    const content = (await import(href)).default
+    return content
   } catch (error) {
     try {
       return loadJsSync(path)
@@ -56,36 +75,11 @@ export async function loadJs<T = any>(path: string): Promise<T> {
 }
 
 /**
- * 加载 ts 文件
+ * 加载 js 文件
  * @param path 文件路径
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function loadTsSync<T = any>(path: string): T {
-  const compiledPath = `${path.slice(0, -2)}cjs`
-  try {
-    const config = resolveTsConfig(dirname(path)) ?? {}
-    config.compilerOptions = {
-      ...config.compilerOptions,
-      module: ModuleKind.NodeNext,
-      moduleResolution: ModuleResolutionKind.NodeNext,
-      target: ScriptTarget.ES2022,
-      noEmit: false,
-    }
-    const transpiledContent = transpileModule(
-      readFileSync(path),
-      config,
-    ).outputText
-    writeFileSync(compiledPath, transpiledContent)
-    return loadJsSync(compiledPath)
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    error.message = `TypeScript Error in ${path}:\n${error.message}`
-    throw error
-  } finally {
-    if (isPathExistsSync(compiledPath)) {
-      removeSync(compiledPath)
-    }
-  }
+export function loadJsSync<T>(path: string): T {
+  return importFresh(path)
 }
 
 /**
@@ -110,17 +104,111 @@ export async function loadTs<T = any>(path: string): Promise<T> {
       const content = await readFile(path)
       transpiledContent = transpileModule(content, config).outputText
       await writeFile(compiledPath, transpiledContent)
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (err: any) {
+    } catch (error) {
+      const err = error as Error
       err.message = `TypeScript Error in ${path}:\n${err.message}`
       throw err
     }
 
-    return await loadJs(compiledPath)
+    return loadJs(compiledPath)
   } finally {
     if (await isPathExists(compiledPath)) {
       await remove(compiledPath)
     }
+  }
+}
+
+/**
+ * 加载 ts 文件
+ * @param path 文件路径
+ */
+export function loadTsSync<T>(path: string): T {
+  const compiledPath = `${path.slice(0, -2)}cjs`
+  try {
+    const config = resolveTsConfig(dirname(path)) ?? {}
+    config.compilerOptions = {
+      ...config.compilerOptions,
+      module: ModuleKind.NodeNext,
+      moduleResolution: ModuleResolutionKind.NodeNext,
+      target: ScriptTarget.ES2022,
+      noEmit: false,
+    }
+    const transpiledContent = transpileModule(
+      readFileSync(path),
+      config,
+    ).outputText
+    writeFileSync(compiledPath, transpiledContent)
+    return loadJsSync(compiledPath)
+  } catch (error) {
+    const err = error as Error
+    err.message = `TypeScript Error in ${path}:\n${err.message}`
+    throw err
+  } finally {
+    if (isPathExistsSync(compiledPath)) {
+      removeSync(compiledPath)
+    }
+  }
+}
+
+/**
+ * 加载 json 文件
+ * @param path 文件路径
+ */
+export async function loadJson<T>(path: string): Promise<T> {
+  const content = await readFile(path)
+
+  try {
+    return parseJson(content)
+  } catch (error) {
+    const err = error as Error
+    err.message = `JSON Error in ${path}:\n${err.message}`
+    throw err
+  }
+}
+
+/**
+ * 加载 json 文件
+ * @param path 文件路径
+ */
+export function loadJsonSync<T>(path: string): T {
+  const content = readFileSync(path)
+
+  try {
+    return parseJson(content)
+  } catch (error) {
+    const err = error as Error
+    err.message = `JSON Error in ${path}:\n${err.message}`
+    throw err
+  }
+}
+
+/**
+ * 加载 yaml 文件
+ * @param path 文件路径
+ */
+export async function loadYaml<T>(path: string): Promise<T> {
+  const content = await readFile(path)
+  try {
+    return yaml.load(content) as T
+  } catch (error) {
+    const err = error as Error
+    err.message = `YAML Error in ${path}:\n${err.message}`
+    throw err
+  }
+}
+
+/**
+ * 加载 yaml 文件
+ * @param path 文件路径
+ */
+export function loadYamlSync<T>(path: string): T {
+  const content = readFileSync(path)
+  try {
+    return yaml.load(content) as T
+  } catch (error) {
+    const err = error as Error
+    err.message = `YAML Error in ${path}:\n${err.message}`
+    throw err
   }
 }
 
@@ -135,6 +223,7 @@ export function resolveTsConfig(dir: string): TranspileOptions {
 
   if (path !== undefined) {
     const { config, error } = readConfigFile(path, path => sys.readFile(path))
+
     if (error) {
       throw new Error(`Error in ${path}: ${error.messageText.toString()}`)
     }
