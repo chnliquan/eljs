cd48461eec81df04ed4a7b295660364cd56e539d
chnliquan <chnliquan@163.com>
2025-03-17 16:36:55 +0800

    chore: bump version v1.0.0-beta.1

---
+++ packages/utils/src/object/merge.ts
@@ -0,0 +1,40 @@
+import deepmerge from 'deepmerge'
+
+export function deepMerge<T1, T2>(a: Partial<T1>, b: Partial<T2>): T1 & T2
+export function deepMerge<T1, T2, T3>(
+  a: Partial<T1>,
+  b: Partial<T2>,
+  c: Partial<T3>,
+): T1 & T2 & T3
+export function deepMerge<T1, T2, T3, T4>(
+  a: Partial<T1>,
+  b: Partial<T2>,
+  c: Partial<T3>,
+  d: Partial<T4>,
+): T1 & T2 & T3 & T4
+export function deepMerge<T1, T2, T3, T4, T5>(
+  a: Partial<T1>,
+  b: Partial<T2>,
+  c: Partial<T3>,
+  d: Partial<T4>,
+  e: Partial<T5>,
+): T1 & T2 & T3 & T4 & T5
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function deepMerge(...sources: any[]) {
+  return sources.reduce((acc, current) => deepmerge(acc, current), {})
+}
+
+console.log(
+  deepMerge(
+    {
+      a: 1,
+    },
+    {
+      b: 2,
+    },
+    {
+      b: 10,
+      c: 3,
+    },
+  ),
+)
